92540c823622aacc6fe3a7ed0bd2c2f1f940261d
stephaniebernhard <fanny.bernhard@gmail.com>
2019-02-28 12:46:25 +0100

    exercise 1 done

--- serie1/serie1.hs
+++ serie1/serie1.hs
@@ -29,7 +29,22 @@ sumSq m n = sum' square m n
 -- i.e. sum' square 3 5
 
 -- AUFGABE 3
--- substring :: String -> String -> Bool
--- substring = 
+substring :: String -> String -> Bool
+substring x y = take (length x) y == x
+-- i.e. substring "abc" "abde"
+-- i.e. substring "abc" "abcde"
 
 -- AUFGABE 4
+replace :: Eq a => [a] -> [a] -> [a] -> [a]
+replace [] _ _ = []
+replace s find repl =
+    if take (length find) s == find
+        then repl ++ (replace (drop (length find) s) find repl)
+        else [head s] ++ (replace (tail s) find repl)
+-- i.e. replace "hello" "he" "!!" 
+
+parens :: String -> String
+parens x = replace x "a" "<a>"
+
+parens2 :: String -> String
+parens2 x = replace x "abc" "<abc>"
